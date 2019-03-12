b15dce26ecc8d7a202e6d00199e6fc8ba94c69b8
[saransekar] <[saran@5gindia.net]>
2019-03-12 19:25:20 -0300

    modified code

--- main.py
+++ main.py
@@ -1,56 +1,3 @@
-""" import todo
-todolist1 = []
-def instruct_todo_task():
-	print("Please select any one below given:\n1.Create ToDo\n2.List ToDo\n3.Update ToDo")
-
-def create_todo():
-	todo_input = input("Enter the your task:")
-	todo_input = todo.ToDo(todo_input)
-	return todo_input
-
-def display_todo_list(todolist):
-	for todo_val in todolist:
-		print(todolist1.append(todo_val.todo))
-	return(todolist1)
-
-def update_todo(choice,todolist1):
-
-	updated_todo = {}	
-	initial = 0
-	for i in range(1,len(todolist1)+1):	
-		o = ''				
-		updated_todo[i] = "%s%s" % (o, todolist1[initial])
-		initial = initial + 1
-	
-
-	for update_idx,update_val in updated_todo.items():
-
-		if choice == update_idx:
-			print(update_val,"Completed")
- 
-
-if __name__ == '__main__':
-	todolist = []	
-	while True:
-		instruct_todo_task()		
-		option = int(input("Enter Option:"))
-		if option == 1:
-			todo_task = create_todo()
-			todolist.append(todo_task)		
-		if option == 2:
-			todolist1 = display_todo_list(todolist)
-			#print(todolist1)
-		if option == 3:
-			#for todo_idx,todo_val in enumerate(todolist):
-				#print("{}{}{}".format(todo_idx + 1,".",todo_val.todo))
-
-			choice = int(input("Enter choice:"))
-			update_todo(choice,todolist1)	"""
-
-
-
-
-
 import todo
 
 def instruct_todo_task():
@@ -58,25 +5,62 @@ def instruct_todo_task():
 
 def create_todo():
 	task = input("Enter the your task:")
-	task = todo.ToDo(task)
-	return task
+	if task != '':
+		task = todo.ToDo(task)
+		return task	
+	else:
+		print("Please enter your task")
+		
+def display_todo_list(todolist):	
+	for todo_idx, todo_val in enumerate(todolist):
+		#print("{}{}{}{}".format(todo_idx + 1,".",todo_val.task,"- Completed",todo_val.is_complete))
+		print("{}{}{}".format(todo_idx + 1,".",todo_val.task))
 
-def display_todo_list(todolist):
-	for todo_val in todolist:
-		print(todo_val.task)
 
- 
-if __name__ == '__main__':
-	todolist = []	
+def main():
+	todolist = []
+	updatedlist = []	
 	while True:
 		instruct_todo_task()		
 		option = int(input("Enter Option:"))
 		if option == 1:
 			todo_task = create_todo()
-			todolist.append(todo_task)		
-		if option == 2:
-			display_todo_list(todolist)			
-		if option == 3:
-			choice = int(input("Enter choice:"))
-			todo_task.update_task(choice,todolist)	
-			print(todo_task.choice)
+			if todo_task is None:
+				todolist = []
+			else:
+				todolist.append(todo_task)	
+			
+		elif option == 2:
+			if todolist != []:
+				display_todo_list(todolist)
+			else:
+				print("Please create todo task")
+						
+		elif option == 3:				
+			if todolist != []:
+				choice = int(input("Enter the number:"))
+				if choice <= len(todolist):			
+					todo = todolist[choice-1]					
+					if todo.is_complete:
+						print("Already Completed")
+					else:	
+						todo.update_task()
+						updatedlist.append(todo)			
+						for updatedlist_idx, updatedlist_val in enumerate(updatedlist):
+							print("{}{}{}{}".format(updatedlist_idx + 1,".",updatedlist_val.task,"- Completed",updatedlist_val.is_complete))
+				else:
+					print("Correct choice input")
+			else:
+				print("Please create todo task")
+
+		elif option == 4:
+			exit()
+
+		else:
+			print("Doesn't exist")		
+			 
+if __name__ == '__main__':
+	try:
+		main()
+	except (SyntaxError, ValueError):		
+		print("You didn't enter a number")				
