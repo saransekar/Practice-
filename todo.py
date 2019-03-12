71a8f995b20799609a723178694df6c55129ed56
[saransekar] <[saran@5gindia.net]>
2019-03-12 19:26:30 -0300

    changed code in todo file

--- todo.py
+++ todo.py
@@ -1,16 +1,27 @@
-class ToDo:
+"""class ToDo:
 
 	def __init__(self,task):
 		self.task = task
 
-	def update_task(self,choice,todolist):
-		self.choice = choice
-		if self.choice == len(todolist):
-			print("correct")
+	def update_task(self,updated,todolist):
+		self.__updated = updated
+		updated_task = False
+		if self.__updated <= len(todolist):
+			updated_task = True
+			updated = todolist[self.__updated - 1]
+			#print(updated.__task,"Completed")
+			print(updated.task,"Completed")
 		else:
-			print("wrong")	
-
-			
-		
+			print("Incompleted")"""
 
 
+
+
+class ToDo:
+
+	def __init__(self,task):
+		self.task = task
+		self.is_complete = False 
+
+	def update_task(self):
+		self.is_complete = True
